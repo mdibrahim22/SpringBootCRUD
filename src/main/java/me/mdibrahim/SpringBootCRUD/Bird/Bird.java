b6ee074a698b608cb95a637e6d61c184b0de8b3c
IBRAHIM <md.ibhw@gmail.com>
2023-04-10 15:39:33 +0600

    add new class Bird and define some entity

--- src/main/java/me/mdibrahim/SpringBootCRUD/Bird/Bird.java
+++ src/main/java/me/mdibrahim/SpringBootCRUD/Bird/Bird.java
@@ -4,12 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@NoArgsConstructor
 public class Bird {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer ID;
     private String Name;
     private String Color;
+
+    public Bird(String name, String color) {
+        Name = name;
+        Color = color;
+    }
 }
